7c2b87c017b38fe859ba84dc1a7da164b9a8c2da
Lyon_Rosenblatt <48656065+lyon040502003@users.noreply.github.com>
2024-08-13 17:07:25 +0200

    updates for better usability

--- AyonCiCd/GBench.py
+++ AyonCiCd/GBench.py
@@ -2,18 +2,29 @@ import os
 from . import cmd
 
 
-
-def run_google_benchmark(GBenchPath: str, OutFilePath:str, *args):
-    """ function for running google benchmark (this function will not register errors as benchmarks should not be used for testing)
+def run_google_benchmark(
+    GBenchPath: str, OutFilePath: str, time_unit: str = "ms", *args
+):
+    """function for running google benchmark (this function will not register errors as benchmarks should not be used for testing)
 
     Args:
-        GBenchPath: path to the Gbnech program that you want to run 
+        GBenchPath: path to the Gbnech program that you want to run
         OutFilePath: path to where gbench should output its output file
-        *args: extra arguments to be passed to google bench  
+        time_unit: define the time unit that google bench should output. ns/usd/ms/s
+        *args: extra arguments to be passed to google bench
     """
+    args = list(args)
+    if not time_unit in ["ns", "us", "ms", "s"]:
+        args.append(time_unit)
+        time_unit = "ms"
+
     GBnechPath = os.path.abspath(GBenchPath)
     OutFilePath = os.path.abspath(OutFilePath)
     os.makedirs(os.path.dirname(OutFilePath), exist_ok=True)
-    OutCommand = ["--benchmark_time_unit=ms",f"--benchmark_out={OutFilePath}"]
-    command = [GBnechPath] + OutCommand + list(args)
+
+    OutCommand = [
+        f"--benchmark_time_unit={time_unit}",
+        f"--benchmark_out={OutFilePath}",
+    ]
+    command = [GBnechPath] + OutCommand + args
     cmd.run(command)
