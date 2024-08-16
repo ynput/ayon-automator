324a671e3bf7d71ee8c3f1879654b3324c49a902
Lyon_Rosenblatt <48656065+lyon040502003@users.noreply.github.com>
2024-08-16 15:05:58 +0200

    forgot to remove an old call to Project.setup()

--- AyonCiCd/Project.py
+++ AyonCiCd/Project.py
@@ -119,9 +119,6 @@ class Project:
         self._project_execuition_error_int = 0
         self._is_setup_process = False
 
-        if "setup" in sys.argv:
-            self._is_setup_process = True
-            self.setup()
 
     def __del__(self):
         """function to cast sys.exit(1) if project errors have accrued. this is important as sys.exit(1) will cause github action to flag the run as failed"""
